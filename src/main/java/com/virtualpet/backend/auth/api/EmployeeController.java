af7b456b7d3337bad55aa45b5b1f2a08c6e7f916
Facundo Criado <facu-1999-44@hotmail.com>
2026-05-22 15:35:00 -0300

    refactor(api): reestructurar controladores REST por Bounded Context
    - Se elimina el registro de empleados y clientes del AuthController.
    - Se crea CustomerController para flujos públicos de e-commerce.
    - Se divide la gestión de personal en AdminController (operaciones gerenciales) y EmployeeController (autoservicio y '/me').
    - Se aplican políticas de seguridad estrictas con @PreAuthorize."de los datos en los controladores.

---
+++ src/main/java/com/virtualpet/backend/auth/api/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.virtualpet.backend.auth.api;
+
+
+import com.virtualpet.backend.auth.dto.EmployeeDTO.EmployeeResponse;
+import com.virtualpet.backend.auth.service.registerOrchestrator.EmployeeOrchestrator;
+import com.virtualpet.backend.shared.security.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employees") // Ruta personal / genérica
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeOrchestrator orchestrator;
+
+    @GetMapping("/me")
+    @PreAuthorize("hasAnyRole('EMPLOYEE', 'ADMIN')")
+    public ResponseEntity<EmployeeResponse> getMyProfile(@AuthenticationPrincipal UserPrincipal currentUser) {
+        return ResponseEntity.ok(orchestrator.executeMe(currentUser.getId()));
+    }
+
+    //@GetMapping()
+    //Metodo para editar datos de empleado
+}
