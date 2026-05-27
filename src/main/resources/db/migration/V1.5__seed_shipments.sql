0b8bf0fd52ff4dc5587f1e249012559904846b62
Facundo Criado <facu-1999-44@hotmail.com>
2026-05-27 16:27:11 -0300

    feat(db): deposito mar del plata

---
+++ src/main/resources/db/migration/V1.5__seed_shipments.sql
@@ -0,0 +1,3 @@
+-- Inserción del centro de distribución principal
+INSERT INTO shipments.warehouses (name, city, state, zip_code)
+VALUES ('Depósito Central MdP', 'Mar del Plata', 'Buenos Aires', '7600');
