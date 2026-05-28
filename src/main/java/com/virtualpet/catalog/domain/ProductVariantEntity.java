930010dc7851204b000b5c113729b128c880565a
JuanCruzMateos <juanczmt@gmail.com>
2026-05-27 21:04:47 -0300

    fix src/main/java/com/virtualpet/catalog/domain/ProductVariantEntity.java

--- src/main/java/com/virtualpet/catalog/domain/ProductVariantEntity.java
+++ src/main/java/com/virtualpet/catalog/domain/ProductVariantEntity.java
@@ -44,14 +44,10 @@ public class ProductVariantEntity {
   @Column(nullable = false)
   private int stock;
 
-<<<<<<< HEAD
-  @Column(name = "image_path")
-=======
   @Column(name = "stock_min", nullable = false)
   private int stockMin;
 
   @Column(name = "image_url")
->>>>>>> develop
   private String imageUrl;
 
   @Column(nullable = false)
